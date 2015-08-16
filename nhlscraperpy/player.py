0a73d7e6200900739e93fb2a7d13631bcd02bf2e
Vais Mahendran <vais.mahendran@gmail.com>
2015-08-16 16:52:57 -0400

    added to requirements, worked on scrapeTOI.py

---
+++ nhlscraperpy/player.py
@@ -0,0 +1,9 @@
+"""
+
+    ~~~~~~~~~~~~
+
+    Representation of a player.
+
+    Copyright: (C) 2015 by Vaisnavan Mahendran
+    License: MIT, see LICENSE for more details
+"""
--- nhlscraperpy/scraper/scrapeTOI.py
+++ nhlscraperpy/scraper/scrapeTOI.py
@@ -7,3 +7,28 @@
     Copyright: (C) 2015 by Vaisnavan Mahendran
     License: MIT, see LICENSE for more details
 """
+
+from downloadNHLFiles import get_toi_home, get_toi_visit
+
+class TOI:
+
+    def __init__(self, season, game_id):
+        self.toi_home = get_toi_home(season, game_id)
+        self.toi_visit = get_toi_visit(season, game_id)
+        self.home_team_players = []
+        self.vist_team_players = []
+
+    def _scrape_header_info(self):
+        """Scrapes header information from toi"""
+
+        pass
+
+    def _scrape_home_toi(self):
+        """Scrapes home team toi"""
+
+        pass
+
+    def _scrape_visit_toi(self):
+        """Scrapes visit team toi"""
+
+        pass
