35106c01749b40e0e2d8b997e126413d59572ac9
Vais Mahendran <vais.mahendran@gmail.com>
2015-10-11 17:45:38 -0400

    resetting files -- its disorganized

--- nhlscraperpy/util/util.py
+++
@@ -1,9 +0,0 @@
-"""
-
-    ~~~~~~~~~~~~
-
-    A collection of utility functions that are used throughout the module
-
-    Copyright: (C) 2015 by Vaisnavan Mahendran
-    License: MIT, see LICENSE for more details
-"""
