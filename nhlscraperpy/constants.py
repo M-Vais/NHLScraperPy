dc74a0d3abc68d86b891862e32b0fd6ec257d0e7
Vais Mahendran <vais.mahendran@gmail.com>
2016-01-29 18:54:27 -0800

    added more seasons and season games played per season in constants

--- nhlscraperpy/constants.py
+++ nhlscraperpy/constants.py
@@ -6,7 +6,6 @@
 	
     Copyright: (C) 2015 by Vaisnavan Mahendran
     License: MIT, see LICENSE for more details
-
 """
 
 MODE = {
@@ -21,12 +20,15 @@ GAME_FILES = {
     'ROSTER' : 'http://www.nhl.com/scores/htmlreports/{0}/RO0{1}{2}.HTM',
     'PBP' : 'http://www.nhl.com/scores/htmlreports/{0}/PL0{1}{2}.HTM',
     'TOIV' : 'http://www.nhl.com/scores/htmlreports/{0}/TV0{1}{2}.HTM',
-    'TOIH' : 'http://www.nhl.com/scores/htmlreports/{0}/TH0{1}{2}.HTM'
-
+    'TOIH' : 'http://www.nhl.com/scores/htmlreports/{0}/TH0{1}{2}.HTM' 
 }
 
 SEASONS = {
-	
+
+	2008 : "20072008",
+	2009 : "20082009",
+	2010 : "20092010",
+	2011 : "20102011",
 	2012 : "20112012",
 	2013 : "20122013",
 	2014 : "20132014",
@@ -34,7 +36,20 @@ SEASONS = {
 	2016 : "20152016"
 }
 
-REG_SEASON_GAMES = 1230
+SEASON_GAMES = {
+
+	2008 : 1230,
+	2009 : 1230,
+	2010 : 1230,
+	2011 : 1230,
+	2012 : 1230,
+	2013 :  720,
+	2014 : 1230,
+	2015 : 1230,
+	2016 : 1230,
+}
+
+
 
 
 
