8723f5e1d84c7e48ed2853941d1e937a899b3a0a
Vais Mahendran <vais.mahendran@gmail.com>
2016-03-14 22:48:00 -0400

    Added files, to work on

--- nhlscraperpy/constants.py
+++ nhlscraperpy/constants.py
@@ -15,30 +15,36 @@ MODE = {
 	'PLAYOFF' : '3'
 }
 
-GAME_FILES_HTML = {
+GAME_HTML = {
 
+	# {0} : YYYYZZZZ - YYYY (START_YEAR) - ZZZZ (END_YEAR)
+	# {1} : XX (Game Type)
+	# {2} : WWWW (Game Number)
     'ROSTER' : 'http://www.nhl.com/scores/htmlreports/{0}/RO0{1}{2}.HTM',
     'PBP' : 'http://www.nhl.com/scores/htmlreports/{0}/PL0{1}{2}.HTM',
     'TOIV' : 'http://www.nhl.com/scores/htmlreports/{0}/TV0{1}{2}.HTM',
     'TOIH' : 'http://www.nhl.com/scores/htmlreports/{0}/TH0{1}{2}.HTM' 
 }
 
-GAME_FILES_JSON {
+GAME_JSON {
 	
-	'PBP' : 'https://statsapi.web.nhl.com/api/v1/game/{0}/feed/live' # 2014020001
+	# Contains easy to access information about players playing in games. Will
+	# consider doing a full scraper based on JSON in the future.
+	# {0} : YYYYXXZZZZ - YYYY (Year) - XX (Game Type) - ZZZZ (Game Number)	
+	'PBP' : 'https://statsapi.web.nhl.com/api/v1/game/{0}/feed/live'
 }
 
 SEASONS = {
 
-	2008 : "20072008",
-	2009 : "20082009",
-	2010 : "20092010",
-	2011 : "20102011",
-	2012 : "20112012",
-	2013 : "20122013",
-	2014 : "20132014",
-	2015 : "20142015",
-	2016 : "20152016"
+	2007 : "20072008",
+	2008 : "20082009",
+	2009 : "20092010",
+	2010 : "20102011",
+	2011 : "20112012",
+	2012 : "20122013",
+	2013 : "20132014",
+	2014 : "20142015",
+	2015 : "20152016"
 }
 
 
---
+++ nhlscraperpy/scrapers/scrape_pbp.py
@@ -0,0 +1,1 @@
+# TODO: Add scraper for pbp html
