8565aa220e950d5642cb30c0cf5e3e5a6fd18915
Vais Mahendran <vais.mahendran@gmail.com>
2016-04-03 23:13:02 -0400

    started on scrape_toi

--- nhlscraperpy/scrapers/scrape_toi.py
+++ nhlscraperpy/scrapers/scrape_toi.py
@@ -1,4 +1,12 @@
-# TODO: Add scraper for toi html
+"""
+	scrape_roster.py
+	~~~~~~~~~~~~~
+	scrape_toi.py scrapes the toi information and returns it in a list of
+	list format.
+
+    Copyright: (C) 2016 by Vaisnavan Mahendran
+    License: MIT, see LICENSE for more details
+"""
 
 def scrape_toi(html):
 
