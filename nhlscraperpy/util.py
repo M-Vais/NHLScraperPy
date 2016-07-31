e7e11d79797cca848838dda638691a182117f788
Vais Mahendran <vais.mahendran@gmail.com>
2016-07-30 21:09:42 -0400

    slight changes, removed files and cleaned up stuff for testing

--- nhlscraperpy/util.py
+++ nhlscraperpy/util.py
@@ -1,12 +1,31 @@
-<<<<<<< HEAD
 from .constants import FILES, SEASONS
-from itertools import chain
+import requests
 
-if __name__ == '__main__':
-=======
-from itertools import chain
+class GameID:
 
-def download_html(season, game_type, game_number):
->>>>>>> 0c37e4b01d3fab7443eef839c40c6b64c5091bb8
+    def __init__(self, season, game_type, game_number):
+        self.season = str(season)
+        self.game_type = str(game_type).zfill(2)
+        self.game_number = str(game_number).zfill(4)
 
-    pass
+    def season(self):
+        return self.season
+
+    def game_type(self):
+        return self.game_type
+
+    def game_number(self):
+        return self.game_number
+
+
+def get_url(game_id, filename):
+
+    if filename == 'JSON':
+        return FILES[filename].format(game_id.season + game_id.game_type + game_id.game_number)
+    else:
+        return FILES[filename].format(SEASONS[game_id.season], game_id.game_type, game_id.game_number)
+
+
+def download_file(game_id, filename):
+
+    return requests.get(get_url(game_id, filename), auth=('user', 'pass'))
