2abbfa18f5f7046d9febcf775e375182d4f7ec5e
Vais Mahendran <vais.mahendran@gmail.com>
2017-05-21 10:45:03 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
  setup(name='nhlscraperpy',
  	   version='0.1',
  	   description='Advanced Data for NHL Games',
- 	   author='Vais Mahendran',
+ 	   author='Vaisnavan Mahendran',
  	   author_email='vais.mahendran@gmail.com',
  	   license='MIT',
  	   packages=['nhlscraperpy'],
