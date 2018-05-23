94801963b64448a35007ef29c6111a185fdd6f00
Vais Mahendran <vais.mahendran@gmail.com>
2018-05-22 22:12:36 -0400

    Added util.py
    Also added requests dependency and other tiny structural changes

--- nhlscraperpy/util.py
+++ nhlscraperpy/util.py
@@ -1,3 +1,15 @@
-from .constants import FILES, SEASONS
+import requests
 
+def get_html(url):  
+    try:
+        request = requests.get(url)
 
+        if request.status_code == 200:
+            return request.text
+        else:
+            print('Error 404 - Unable to download: {0}'.format(url))
+            return None
+
+    except requests.exceptions.RequestException:
+        print('Error - Unable to download: {0}'.format(url))
+        return None
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='nhlscraperpy',
  	   version='0.1',
- 	   description='Advanced Data for NHL Games',
+ 	   description='Fetching advanced analytics from NHL Data',
  	   author='Vaisnavan Mahendran',
  	   author_email='vais.mahendran@gmail.com',
  	   license='MIT',
